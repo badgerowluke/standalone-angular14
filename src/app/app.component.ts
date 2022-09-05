cf9ffe36a8bcb43ef6526ff61f3a9200fbf554f3
Luke Badgerow <badgerow.luke@gmail.com>
2022-09-05 14:32:10 -0400

    pushing an initial working of a stand-alone app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Map } from 'leaflet';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'standalone-example';
+  private map: Map;
+  private zoom: number;
+  
+  receiveMap(map: Map) {
+    this.map = map;
+  }
+
+  receiveZoom(zoom: number) {
+    this.zoom = zoom;
+  }
 }
